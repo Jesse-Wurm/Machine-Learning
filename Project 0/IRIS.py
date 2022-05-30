d817301b5d8f600a54d06b9979d6305e8ec5464f
Jesse-Wurm <jesse.wurm@outlook.com>
2022-05-30 11:58:18 -0800

    Starting file with dependancies

---
+++ Project 0/IRIS.py
@@ -0,0 +1,7 @@
+#import libraries
+import sklearn as sk
+from sk import datasets
+from sk.model_selection import train_test_split
+from sk.ensemble import RandomForestClassifier
+from sk.datasets import make_classification
+
---
+++ Project 0/README.md
@@ -0,0 +1,4 @@
+Quick Classification model
+
+
+I will be using an example of an IRIS dataset. I will be using classifcation - Random Forest model.
