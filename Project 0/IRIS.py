6666a9531ba0c1534093ea7d7f828db2aaf9732d
Jesse-Wurm <jesse.wurm@outlook.com>
2022-05-30 12:15:08 -0800

    Fixed the issues with importing the scikit-learn library

--- Project 0/IRIS.py
+++ Project 0/IRIS.py
@@ -1,7 +1,27 @@
 #import libraries
-import sklearn as sk
-from sk import datasets
-from sk.model_selection import train_test_split
-from sk.ensemble import RandomForestClassifier
-from sk.datasets import make_classification
+# I had to change how the dataset was imported as I was using an other example
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+
+#Fixed the import libraries to work with sklearn
+
+# load the IRIS dataset
+iris = datasets.load_iris()
+
+# import features and labels
+
+#print tehe IRIS feature names
+print(iris.feature_names)
+
+# that will print the names of the IRIS features, next we are going print the target names
+
+#print the IRIS target names
+print(iris.target_names)
+
+#lets look at that dataset
+
+#print the whole dataset
+print(iris.data)
 
