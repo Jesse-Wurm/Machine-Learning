e6a143e1bd78bec39039dd6346fdfeca152de7fe
Jesse-Wurm <jesse.wurm@outlook.com>
2022-05-30 13:02:12 -0800

    Completed model and recieved score

--- Project 0/IRIS.py
+++ Project 0/IRIS.py
@@ -25,3 +25,44 @@ print(iris.target_names)
 #print the whole dataset
 print(iris.data)
 
+#thats a lot of data, now lets look at the target "Class labels"
+print(iris.target)
+
+#now that we've seen the data, lets assign it to variables
+x = iris.data
+y = iris.target
+
+#Lets print the data dimensions
+print(x.shape)
+print(y.shape)
+
+#Building a Classification Mdel with Random Forest
+clf = RandomForestClassifier()
+
+print(clf.fit(x, y))
+
+
+print(clf.feature_importances_)
+
+#predictions
+
+x[0]
+
+print(clf.predict([[5.1, 3.5, 1.4, 0.2]]))
+print(clf.predict(x[[0]]))
+print(clf.predict_proba(x[[0]]))
+clf.fit(iris.data, iris.target_names[iris.target])
+
+#split the data into 80/20 train/test sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+x_train.shape, y_train.shape, x_test.shape, y_test.shape
+
+#rebuild the model
+clf.fit(x_train, y_train)
+print(clf.predict([[5.1, 3.5, 1.4, 0.2]]))
+print(clf.predict_proba([[5.1, 3.5, 1.4, 0.2]]))
+print(clf.predict(x_test))
+print(y_test)
+
+#model preformance evaluation
+print(clf.score(x_test, y_test))
